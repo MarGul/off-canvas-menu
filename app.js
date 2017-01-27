c91e12f7c43a9e5aae49262cb340bdb7418ec9d7
Marcus Gullberg <marcus.gullberg@resurs.se>
2017-01-27 16:05:36 +0100

    Initial upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var mobileToggle = document.getElementById('mobile-nav-toggle');
+
+mobileToggle.addEventListener("click", function(event) {
+	if ( document.body.classList.contains('mobile-nav-open') ) {
+		document.body.classList.remove('mobile-nav-open');
+	} else {
+		document.body.classList.add('mobile-nav-open');
+	}
+});
