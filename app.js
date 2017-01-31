53649bda14d99260985ddbc5dfbd5c7bd79e2718
Marcus Gullberg <marcus.gullberg@resurs.se>
2017-01-31 16:37:02 +0100

    Added menu-nav-toggle animation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var mobileToggle = document.getElementById('mobile-nav-toggle');
 
 mobileToggle.addEventListener("click", function(event) {
+	event.preventDefault();
 	if ( document.body.classList.contains('mobile-nav-open') ) {
 		document.body.classList.remove('mobile-nav-open');
 	} else {
